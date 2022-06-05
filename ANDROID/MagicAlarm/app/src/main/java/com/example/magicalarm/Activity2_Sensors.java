022d6229efd1b5496f4e87f10a1be35fb1afc933
Jimena B. Gonzalez <jibego2013@gmail.com>
2022-06-04 22:43:50 -0300

    v1.1 ==> Se arregla lampara para que solo se coloree si light=0 y otros minimos cambios

---
+++ ANDROID/MagicAlarm/app/src/main/java/com/example/magicalarm/Activity2_Sensors.java
@@ -0,0 +1,207 @@
+package com.example.magicalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+
+//Communication
+import android.content.Intent;
+
+// Sensor Management
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+// Logging
+import android.util.Log;
+
+// UI Components
+import android.graphics.Color;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.text.DecimalFormat;
+
+public class Activity2_Sensors extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private TextView      accelerometer;
+    private TextView      gyroscope;
+    private TextView      lightness;
+    private TextView      giro;
+    private ImageView     lightBulbImg;
+    DecimalFormat float2 = new DecimalFormat("###.###");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        // Defino los botones
+
+        // Defino los TXT para representar los datos de los sensores
+        accelerometer  = (TextView) findViewById(R.id.accelerometer);
+        gyroscope     = (TextView) findViewById(R.id.gyroscope);
+        lightness   = (TextView) findViewById(R.id.lightness);
+        giro          = (TextView) findViewById(R.id.giro);
+        lightBulbImg = (ImageView) findViewById(R.id.lightBulbimageView);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE); // Acceder al servicio de sensores
+
+        //showSensorOutput();
+    }
+
+    /*private void showSensorOutput() {
+        Intent iCommunicate = new Intent();
+        iCommunicate.setClass(Activity2_Sensors.this, SensorListActivity.class);
+
+        startActivity(iCommunicate);
+    }*/
+
+    protected void initializeSensors() { // Iniciar el acceso a los sensores
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),   SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),       SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),           SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),         SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    private void stopSensorListening() { // Finalizar escucha de sensores
+
+        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE));
+        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR));
+        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT));
+        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY));
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { // Escuchar cambio de sensibilidad de los sensores
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) { // Escuchar cambio en los sensores
+
+        String txt = "";
+
+        // Cada sensor puede lanzar un thread que pasa por aca ==> Accesos Simultaneos
+        // Accesos Simultaneos ==> Sincronizar por seguridad
+        synchronized (this) {
+
+            Log.d("sensor", event.sensor.getName());
+
+            switch(event.sensor.getType()) {
+
+                case Sensor.TYPE_ACCELEROMETER :
+                    txt += "--- Acelerometro ---\n";
+                    txt += "Coord. x = " + float2.format(event.values[0]) + " m/seg2 \n";
+                    txt += "Coord. y = " + float2.format(event.values[1]) + " m/seg2 \n";
+                    txt += "Coord. z = " + float2.format(event.values[2]) + " m/seg2 \n";
+                    if ((event.values[0] > 25) || (event.values[1] > 25) || (event.values[2] > 25)) {
+                        txt += "Vibracion Detectada \n";
+                    }
+
+                    accelerometer.setText(txt);
+
+                    break;
+
+                case Sensor.TYPE_GYROSCOPE:
+                    txt += "--- Giroscopo ---\n";
+                    txt += "Coord. x = " + float2.format(event.values[0]) + " deg/s \n";
+                    txt += "Coord. y = " + float2.format(event.values[1]) + " deg/s \n";
+                    txt += "Coord. z = " + float2.format(event.values[2]) + " deg/s \n";
+
+                    gyroscope.setText(txt);
+
+                    break;
+
+                case Sensor.TYPE_ROTATION_VECTOR :
+                    txt += "--- Vector de rotacion ---\n";
+                    txt += "Coord. x = " + event.values[0] + "\n";
+                    txt += "Coord. y = " + event.values[1] + "\n";
+                    txt += "Coord. z = " + event.values[2] + "\n";
+
+                    // Creo objeto para saber como está la pantalla
+                    Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+                    int rotation = display.getRotation();
+
+                    // El objeto devuelve 3 estados 0, 1 y 3
+                    switch(rotation) {
+                        case 0:
+                            txt += "Posicion = Vertical \n";
+                            break;
+                        case 1:
+                            txt += "Posicion = Horizontal Izq. \n";
+                            break;
+                        case 2:
+                            txt += "Posicion = Horizontal Der \n";
+                            break;
+                    }
+
+                    txt += "Display: " + rotation + "\n";
+                    giro.setText(txt);
+
+                    break;
+
+                case Sensor.TYPE_LIGHT :
+                    txt += "--- Luminosidad ---\n";
+                    txt += event.values[0] + " Luz \n";
+
+                    lightness.setText(txt);
+
+                    if(event.values[0] <= 0) {
+                        System.out.println("No se detecta luz ==> PRENDER LUZ"); /** DEBUG !! **/
+                        lightBulbImg.setColorFilter(Color.parseColor("#FFFFEB3B")); // Si no detectó luz ==> pintar borde de lampara simulando encender luz
+                        //lightBulbImg.setBackgroundColor(Color.parseColor("#FFFFEB3B")); // Si no detectó luz ==> pintar fondo de lampara simulando encender luz
+                    } else {
+                        System.out.println("Se detectó luz ==> APAGAR LUZ"); /** DEBUG !! **/
+                        lightBulbImg.setColorFilter(Color.parseColor("#000000")); // Si detectó luz ==> pintar borde de lampara simulando apagar luz
+                    }
+
+                    break;
+
+                default:
+                    Log.d("UI Sensor ==>","Por defecto --> ningun sensor");
+                    break;
+
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializeSensors();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initializeSensors();
+    }
+
+    @Override
+    protected void onRestart() {
+        initializeSensors();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        stopSensorListening();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopSensorListening();
+        super.onDestroy();
+    }
+
+}
---
+++ ANDROID/MagicAlarm/app/src/main/java/com/example/magicalarm/MainActivity_MagicAlarm.java
@@ -0,0 +1,95 @@
+package com.example.magicalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+// UI Components
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity_MagicAlarm extends AppCompatActivity {
+
+    private Button submitBtn, sensorBtn;
+    private RadioGroup songList;
+    private RadioButton selectedSong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Estado de la Activity: <<onCreate>>"); /** DEBUG !! **/
+
+        submitBtn = (Button) findViewById(R.id.buttonSubmit);
+        sensorBtn = (Button) findViewById(R.id.buttonSensores);
+        songList = (RadioGroup) findViewById(R.id.dropdownSongs); // obtener RadioGroup
+
+
+        sensorBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Se hizo clic en el button <<SENSOR>>"); /** DEBUG !! **/
+                Intent i = new Intent();
+                i.setClass(MainActivity_MagicAlarm.this, Activity2_Sensors.class);
+                startActivity(i);
+            }
+        });
+
+        selectedSong = (RadioButton)songList.findViewById(songList.getCheckedRadioButtonId()); //Obtener RadioButton seleccionado
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = songList.getCheckedRadioButtonId();
+                //we are defining the selectId and then we are fetching the id of the checked radio button using the function getCheckedRadioButton()
+                selectedSong = findViewById(selectedId);
+
+                switch(selectedId) { // Verificar qué radio button se activó
+                    case R.id.song_HarryPotter:
+                        System.out.println("Se hizo clic en la cancion <<song_HarryPotter>>"); /** DEBUG !! **/
+                        // logica envio comando cancion 1
+                        break;
+                    case R.id.song_2:
+                        System.out.println("Se hizo clic en la cancion <<song_2>>"); /** DEBUG !! **/
+                        // logica envio comando cancion 2
+                        break;
+                    case R.id.song_3:
+                        System.out.println("Se hizo clic en la cancion <<song_3>>"); /** DEBUG !! **/
+                        // logica envio comando cancion 3
+                        break;
+                    default:
+                        System.out.println("ERROR! Se debe seleccionar una cancion."); /** DEBUG !! **/
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        System.out.println("Estado de la Activity: <<onStart>>"); /** DEBUG !! **/
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Estado de la Activity: <<onResume>>"); /** DEBUG !! **/
+    }
+
+    @Override
+    protected void onStop() {
+        System.out.println("Estado de la Activity: <<onStop>>"); /** DEBUG !! **/
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        System.out.println("Estado de la Activity: <<onDestroy>>"); /** DEBUG !! **/
+        super.onDestroy();
+    }
+
+
+}
